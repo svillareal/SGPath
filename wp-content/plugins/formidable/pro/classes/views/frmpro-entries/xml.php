de5d976cb75ea67ad3b2ca79b340b1fad5fbcea3
Sherilyn Villareal <me.sherilyn.villareal@gmail.com>
2015-03-17 09:45:48 -0500

    delete old formidable files

--- wp-content/plugins/formidable/pro/classes/views/frmpro-entries/xml.php
+++
@@ -1,7 +0,0 @@
-<script type="text/javascript">
-window.onload=function(){location.href="<?php echo admin_url('admin-ajax.php') ?>?action=frm_<?php echo esc_attr( $controller ) ?>_xml&ids=<?php echo $ids;
-if ( isset( $is_template ) ) {
-    echo '&is_template='. $is_template;
-}
-?>";}
-</script>
